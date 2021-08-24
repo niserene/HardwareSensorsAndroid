7712e99d6faa687b8afdc90df54946d64e487c7b
nishantsahu961 <nishantsahu961@gmail.com>
2021-08-24 10:17:43 +0530

    get the list of hw sensor

--- app/src/main/java/com/example/hardwaresensors/MainActivity.kt
+++ app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.hardwaresensors
 
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sensorManager = getSystemService<SensorManager>()
+        if(sensorManager == null){
+            Toast.makeText(this, "Could not get the sensors", Toast.LENGTH_LONG).show()
+            finish()
+        }
+        else{
+            val sensors:MutableList<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
+            sensors.forEach{
+                Log.d("HWS", "${it.name} | ${it.type}  |  ${it.vendor}")
+            }
+        }
     }
 }
